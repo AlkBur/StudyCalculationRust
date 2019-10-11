15d63dfcd0340d36fbe820dbf84528e2ea396122
BurA <alkboor@mail.ru>
2019-10-11 17:44:19 +0300

    Task: 12

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod task1; mod task2; mod task3; mod task4;
 mod task5; mod task6; mod task7; mod task8;
-mod task9; mod task10; mod task11;
+mod task9; mod task10; mod task11; mod task12;
 
 use std::env;
+
+//export RUSTFLAGS='-Ctarget-cpu=native'
 
 fn main() {
     let mut num_task = 0;
@@ -18,30 +20,20 @@ fn main() {
         5 => task5::task(), 6 => task6::task(),
         7 => task7::task(), 8 => task8::task(),
         9 => task8::task(), 10 => task10::task(),
-        11 => task10::task(),
+        11 => task10::task(), 12 => task12::task(),
         _ => {
             task1::task();
-            println!("{}", "-----------");
-            task2::task();
-            println!("{}", "-----------");
-            task3::task();
-            println!("{}", "-----------");
-            task4::task();
-            println!("{}", "-----------");
-            task5::task();
-            println!("{}", "-----------");
-            task6::task();
-            println!("{}", "-----------");
-            task7::task();
-            println!("{}", "-----------");
-            task8::task();
-            println!("{}", "-----------");
-            task9::task();
-            println!("{}", "-----------");
-            task10::task();
-            println!("{}", "-----------");
-            task11::task();
-            println!("{}", "-----------");
+            println!("{}", "-----------"); task2::task();
+            println!("{}", "-----------"); task3::task();
+            println!("{}", "-----------"); task4::task();
+            println!("{}", "-----------"); task5::task();
+            println!("{}", "-----------"); task6::task();
+            println!("{}", "-----------"); task7::task();
+            println!("{}", "-----------"); task8::task();
+            println!("{}", "-----------"); task9::task();
+            println!("{}", "-----------"); task10::task();
+            println!("{}", "-----------"); task11::task();
+            println!("{}", "-----------"); task12::task();
         }
     }
 }
---
+++ src/task12.rs
@@ -0,0 +1,15 @@
+pub fn task(){
+    println!("Num task: {}", 12);
+    println!("{}", "-----------");
+
+    print(3.6);
+}
+
+fn print(x:f64){
+    let e = 1.0_f64.exp();
+    let mut val = e.powf(x-2.0);
+    val += f64::abs(f64::sin(x));
+    val -= f64::powf(x, 4.0)*f64::cos(1.0/x);
+
+    println!("e^(x−2)+|sin(x)|−x^4⋅cos(1/x)={}", val);
+}
